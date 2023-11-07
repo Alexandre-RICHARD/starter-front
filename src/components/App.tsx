8c25646daed89a77f2d75efdd20575efa046cc95
Alexandre RICHARD <alexandre.richard.dev@gmail.com>
2023-11-07 07:21:13 +0100

    :truck::heavy_plus_sign: Add sass and prepare Redux and Jest

---
+++ src/components/App.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./App.scss";
+// import type { RootState } from "../store/store.ts";
+// import { useSelector, useDispatch } from "react-redux";
+// import { increment } from "../store/slice/globalSlice.ts";
+
+// export const sum = (a : number, b : number) : number => {
+//     return a + b;
+// };
+
+function App() {
+    // const startingValue = useSelector(
+    //     (state: RootState) => state.global.startingValue
+    // );
+    // const dispatch = useDispatch();
+
+    return (
+        <div id="app">
+            {/* <button onClick={() => dispatch(increment())}>
+                Nombre de clic : {startingValue}
+            </button> */}
+            <p>Un début intéressant</p>
+        </div>
+    );
+}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App.tsx";
+import "./styles/index.scss";
+// import { store } from "./store/store.ts";
+// import { Provider } from "react-redux";
 
-ReactDOM.createRoot(document.getElementById('app')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("app")!).render(
+    <React.StrictMode>
+        {/* <Provider store={store}> */}
+            <App />
+        {/* </Provider> */}
+    </React.StrictMode>
+);
