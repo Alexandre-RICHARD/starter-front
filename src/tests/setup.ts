9223b052d451999da55d97cab759ea2a13aeafb0
Alexandre RICHARD <alexandre.richard.dev@gmail.com>
2023-11-18 11:34:38 +0100

    :wastebasket: Clean setup.ts (vitest) and remove jest-dom code, present because confusion with jsdom

--- src/tests/setup.ts
+++ src/tests/setup.ts
@@ -1,13 +1,3 @@
-// import "@testing-library/jest-dom";
-// import { expect, afterEach } from "vitest";
-// import { cleanup } from "@testing-library/react";
-// import matchers from "@testing-library/jest-dom/matchers";
-
-// expect.extend(matchers);
-// afterEach(() => {
-//     cleanup();
-// });
-
 import "@testing-library/jest-dom";
 import { afterEach } from "vitest";
 import { cleanup } from "@testing-library/react";
