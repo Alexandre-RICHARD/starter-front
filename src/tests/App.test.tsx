718fac3b87b7b7655e943e7842157fc308b6269a
Alexandre RICHARD <alexandre.richard.dev@gmail.com>
2023-11-21 19:58:09 +0100

    Temp, merging with last commit

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -6,23 +6,15 @@ import { store } from "@store/store.ts";
 import { Provider } from "react-redux";
 
 describe("<App />", () => {
-    // test("App mounts properly", () => {
-    //     render(<Provider store={store}><App /></Provider>);
-    //     const tested = screen;
-    //     expect(tested).toBeTruthy();
-    // });
-
-    // test("App contains title", () => {
-    //     render(<Provider store={store}><App /></Provider>);
-    //     const tested = screen.getByText("C'est l'heure d'un nouveau projet");
-    //     expect(tested).toBeInTheDocument();
-    // });
-
     test("App mounts properly", () => {
         render(<Provider store={store}><App /></Provider>);
-        const component = screen;
-        expect(component).toBeTruthy();
-        const title = screen.getByText("C'est l'heure d'un nouveau projet");
-        expect(title).toBeInTheDocument();
+        const tested = screen;
+        expect(tested).toBeTruthy();
+    });
+
+    test("App contains title", () => {
+        render(<Provider store={store}><App /></Provider>);
+        const tested = screen.getByText("C'est l'heure d'un nouveau projet");
+        expect(tested).toBeInTheDocument();
     });
 });
